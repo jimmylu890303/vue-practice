0a7359c14ec0aba299aaf09ed53acd89987d80c6
jimmy.lu <jimmy.lu@wizigo.tw>
2022-09-13 10:24:19 +0800

    use css to create postcard

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,4 +1,5 @@
 <template>
+  <!----
   <div>
     <div>
       <b-container>
@@ -31,6 +32,27 @@
       </b-container>
     </div>
   </div>
+-->
+  <div>
+    <div class="conatainer">
+      <h2></h2>
+      <div>
+        <input class="search-input" placeholder="文章搜尋" />
+        <button class="search-btn">search</button>
+      </div>
+      <span>{{ postCount }}則貼文</span>
+      <div class="post-cards">
+        <div class="post" v-for="post in cardlist" :key="post.id">
+          <img class="post-img" src="https://picsum.photos/600/300/?image=25" />
+          <h1 class="post-title">{{ post.title }}</h1>
+          <p class="post-content">
+            {{ post.body }}
+          </p>
+          <button class="post-btn" href="#" variant="primary">see more</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -55,3 +77,73 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.search-input {
+  margin-top: 30px;
+  margin-bottom: 30px;
+  height: 50px;
+  width: 70%;
+  padding: 15px;
+}
+.search-btn {
+  display: block;
+  margin: auto;
+  width: 7%;
+  padding: 10px;
+  margin-bottom: 30px;
+  text-align: center;
+  border-style: solid;
+  border-width: medium;
+  border-color: lightblue;
+  border-radius: 10px;
+  background-color: aquamarine;
+  font-size: 30px;
+  font-weight: bold;
+  color: green;
+}
+div.post {
+  border-style: solid;
+  border-width: medium;
+  border-color: lightblue;
+  border-left-color: lightgreen;
+  border-left-width: 20px;
+  border-radius: 10px;
+  margin: 20px;
+  padding: 10px;
+  width: 20%;
+  display: inline-block;
+  height: 600px;
+}
+.post-img {
+  width: 100%;
+}
+.post-title {
+  text-align: center;
+  color: green;
+  text-shadow: 2px 2px 5px greenyellow;
+  font-weight: bold;
+  font-size: 45px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+.post-content {
+  height: 30%;
+  font-size: 20px;
+  overflow: auto;
+}
+.post-btn {
+  background-color: blue;
+  border-radius: 10px;
+  width: 30%;
+  color: white;
+  padding: 10px 5px 10px 5px;
+  margin-bottom: 10px;
+  font-size: 25px;
+}
+.post-btn:hover {
+  opacity: 0.7;
+  width: 40%;
+}
+</style>
